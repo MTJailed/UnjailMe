5b519bdf78c7db652acd99a627e8cecf5756b133
Abdulsamet Saylık <smtsylk@Abdulsamet-Mac.local>
2018-03-03 02:06:18 -0800

    Webserver on port 80

---
+++ unjailme/webserver.c
@@ -0,0 +1,9 @@
+//
+//  webserver.c
+//  unjailme
+//
+//  Created by Abdulsamet Saylık on 3.03.2018.
+//  Copyright © 2018 Jailed Inc. All rights reserved.
+//
+
+#include "webserver.h"
---
+++ unjailme/webserver.h
@@ -0,0 +1,14 @@
+//
+//  webserver.h
+//  unjailme
+//
+//  Created by Abdulsamet Saylık on 3.03.2018.
+//  Copyright © 2018 Jailed Inc. All rights reserved.
+//
+
+#ifndef webserver_h
+#define webserver_h
+
+#include <stdio.h>
+
+#endif /* webserver_h */
