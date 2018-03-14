6e8ca867b9c17785fb8cf8a24b83d9d3b5f464b8
MTJailed <marketing@jailed.ml>
2018-03-14 13:48:22 +0100

    Create endianhelper.h

---
+++ unjailme/utils/endianhelper.h
@@ -0,0 +1,15 @@
+//
+//  endianhelper.h
+//  unjailme
+//
+//  Created by Sem Voigtländer on 13/03/2018.
+//  Copyright © 2018 Jailed Inc. All rights reserved.
+//
+
+#ifndef endianhelper_h
+#define endianhelper_h
+
+#include <stdio.h>
+int64_t swap_int64( int64_t val );
+uint64_t swap_uint64( uint64_t val );
+#endif /* endianhelper_h */
