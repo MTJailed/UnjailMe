cfa00054ce0012a87c3759cacb54413100e11ca4
MTJailed <marketing@jailed.ml>
2018-03-14 13:49:00 +0100

    Create BaseBoard.h

---
+++ unjailme/BaseBoard.h
@@ -0,0 +1,11 @@
+//
+//  BaseBoard.h
+//  unjailme
+//
+//  Created by Sem Voigtländer on 13/03/2018.
+//  Copyright © 2018 Jailed Inc. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+@interface FAKESELECTORS : NSObject
+-(BOOL)createJobWithLabel:(id)arg1 bundleIdentifier:(id)arg2 path:(id)arg3 containerPath:(id)arg4 arguments:(id)arg5 environment:(id)arg6 standardOutputPath:(id)arg7 standardErrorPath:(id)arg8 machServices:(id)arg9 threadPriority:(long long)arg10 waitForDebugger:(BOOL)arg11 denyCreatingOtherJobs:(BOOL)arg12 runAtLoad:(BOOL)arg13 disableASLR:(BOOL)arg14 systemApp:(BOOL)arg15;
+@end
