dea598de1bdf9843b87bdc10685b6234a8a65c4a
MTJailed <marketing@jailed.ml>
2018-03-14 13:45:42 +0100

    Update AppDelegate.h

--- unjailme/AppDelegate.h
+++ unjailme/AppDelegate.h
@@ -8,13 +8,17 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
-
+#import <Foundation/Foundation.h>
+#import <ReplayKit/ReplayKit.h>
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
 @property (readonly, strong) NSPersistentContainer *persistentContainer;
 
+@property (strong, nonatomic) RPScreenRecorder *screenRecorder;
+@property (strong, nonatomic) AVAssetWriter *assetWriter;
+@property (strong, nonatomic) AVAssetWriterInput *assetWriterInput;
 - (void)saveContext;
 
 
